7ad3de27f53ce3151ee944d644cabe50183e6092
AAARTEMYEV <aaartemyev@tsconsulting.ru>
2018-02-22 18:31:14 +0300

    добавление Triple из appach common lang

--- src/main/java/ru/artemiev/task2/DataStorage.java
+++ src/main/java/ru/artemiev/task2/DataStorage.java
@@ -1,15 +1,30 @@
 package ru.artemiev.task2;
 
 import javafx.util.Pair;
+import org.apache.commons.lang3.tuple.Triple;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class DataStorage {
+
     public static void main(String[] args) {
         DatasReader reader = new DatasReader();
         List<Pair<Integer, String>> dataA = reader.getInfo(args[0]);
-        for (Pair p:
-             dataA) {
-            System.out.println(p);
+//        List<Pair<Integer, String>> dataB = reader.getInfo(args[1]);
+
+    }
+
+    public static  <Integer, String, String> List<Triple<Integer, String, String>> nestedLoopsJoin(List<Pair<Integer, String>> dataA, List<Pair<Integer, String>> dataB) {
+        List<Triple<Integer, String, String>> result = new ArrayList<>();
+        for (Pair<Integer, String> leftPair: dataA) {
+            for (Pair<Integer, String> rightPair: dataB) {
+                if (Objects.equals(leftPair.getKey(), rightPair.getKey())) {
+                    result.add(new Triple<>(leftPair.getKey(), leftPair.getValue(), rightPair.getValue()));
+                }
+            }
         }
+        return result;
     }
 }
---
+++ src/main/java/ru/artemiev/task2/Triple.java
@@ -0,0 +1,10 @@
+package ru.artemiev.task2;
+
+import java.util.ArrayList;
+
+public class Triple<K,T,V> {
+    ArrayList n1, n2, n3;
+    Triple(Integer i, String s1, String s2){
+
+    }
+}
